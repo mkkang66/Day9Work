9b3bb94e7aa9ed73dc9f2d732e62609b47236453
mkkang66 <63625148+mkkang66@users.noreply.github.com>
2020-05-18 15:22:19 +0900

    박경원/강민구 Day9 과제
    박경원/강민구 Day9 과제

---
+++ author.sql
@@ -0,0 +1,53 @@
+-- MariaDB dump 10.17  Distrib 10.5.3-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: web
+-- ------------------------------------------------------
+-- Server version	10.5.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `author`
+--
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `profile` varchar(200) DEFAULT NULL,
+  `password` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (14,'aaaa','aaaa','61be55a8e2f6b4e172338bddf184d6dbee29c98853e0a0485ecee7f27b9af0b4'),(15,'bbbb','bbbb','81cc5b17018674b401b42f35ba07bb79e211239c23bffe658da1577e3e646877'),(16,'gdhong','gdhong','c498bfddca6509cfa48c683b833728e57edca6ed0ce1c0e6176b9ad2d437dddd');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-18 15:16:29
---
+++ work9-1.py
@@ -0,0 +1,234 @@
+from flask import Flask, render_template, request, session, redirect
+from bs4 import BeautifulSoup as BS
+import requests
+import re
+import os
+import pymysql
+from selenium import webdriver
+from konlpy.tag import Kkma
+import base64
+
+if __name__ == "__main__":
+    # Flask 실행 환경 설정
+    app = Flask(__name__,
+                template_folder="./template",
+                static_folder="./static")
+    app.env = "development"
+    app.debug = True
+    app.secret_key = "sldkjfsld"
+
+    # Selenium Web Driver 설정
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome("./chromedriver", options=options)
+
+    # 한글 형태소 분석 엔진 설정
+    kkma = Kkma()
+
+    # MySQL 클라이언트 설정
+    db = pymysql.connect(
+        user="root",
+        passwd="new123",
+        db="web",
+        host="localhost",
+        charset="utf8",
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+    @app.route("/")
+    def index():
+        # 사용자 로그인 확인
+        is_logged_in = True if session.get("user") else False
+
+        if is_logged_in:
+            return render_template(
+                "index.html",
+                is_logged_in=is_logged_in,
+                user=session.get("user").get("name"),
+            )
+
+        return render_template("index.html", is_logged_in=is_logged_in)
+
+    @app.route("/login", methods=["GET", "POST"])
+    def login():
+        if request.method == "GET":
+            return render_template("login.html")
+
+        # 로그인 요청 시 DB에서 사용자 정보 확인
+        cursor = db.cursor()
+        cursor.execute(f"""
+            SELECT id, name, profile FROM author
+            WHERE name = '{ request.form['userid'] }' AND
+                password = SHA2('{ request.form['password'] }', 256)
+        """)
+
+        # 로그인 성공
+        user = cursor.fetchone()
+        if user:
+            # 세션 정보 저장
+            session["user"] = user
+            return redirect("/")
+
+        # 로그인 실패
+        return render_template("login.html", msg="로그인정보를 확인 요망.")
+
+    @app.route("/logout")
+    def logout():
+        # 세션 정보 삭제
+        session.pop("user")
+
+        return redirect("/")
+
+    @app.route("/join", methods=["GET", "POST"])
+    def join():
+        if request.method == "GET":
+            return render_template("join.html")
+
+        # 사용자 정보 입력
+        userid = request.form.get("userid")
+        password = request.form.get("password")
+        profile = request.form.get("profile")
+
+        # 중복 검사
+        cursor = db.cursor()
+        cursor.execute(f"""SELECT id FROM author WHERE name = '{ userid }'""")
+        res = cursor.fetchone()
+        if res:
+            return render_template("join.html", msg="ID 중복입니다. 다시 가입하세요.")
+
+        # 신규 등록
+        query = f"""INSERT INTO `author` VALUES (id, '{ userid }', '{ password }', SHA2('{ profile }', 256));"""
+        cursor = db.cursor()
+        cursor.execute(query)
+        db.commit()
+
+        # 메인 페이지로 이동
+        return redirect("/")
+
+    @app.route("/withdrawal", methods=["GET", "POST"])
+    def withdrawal():
+        if request.method == "GET":
+            return render_template("withdrawal.html")
+
+        # 탈퇴할 사용자 정보
+        userid = session.get("user").get("name")
+        password = request.form.get("password")
+
+        # ID, 비밀번호 검사
+        cursor = db.cursor()
+        cursor.execute(f"""
+            SELECT id FROM author
+            WHERE name = '{ userid }' AND password = SHA2('{ password }', 256)
+        """)
+
+        # 사용자 삭제
+        user = cursor.fetchone()
+        if user:
+            query = f"""DELETE FROM author WHERE name = '{ userid }'"""
+            cursor = db.cursor()
+            cursor.execute(query)
+            db.commit()
+
+            # 세션 정보 삭제
+            session.pop("user")
+
+            # 메인 페이지로 이동
+            return redirect("/")
+
+        # 사용자 삭제 실패
+        return render_template("withdrawal.html", msg="비밀번호를 다시 입력하세요.")
+
+    @app.route("/news/ranking", methods=["GET", "POST"])
+    def news_ranking():
+        if request.method == "GET":
+            return render_template("news_ranking.html")
+
+        url = f"https://media.daum.net/ranking/?regDate={ request.form.get('date') }"
+        driver.get(url)
+
+        soup = BS(driver.page_source, "html.parser")
+
+        news_links = [
+            (tag.get("href"), tag.get_text())
+            for tag in soup.select(".rank_news strong.tit_thumb a.link_txt")
+        ]
+
+        return render_template("news_ranking.html", msg=news_links)
+
+    @app.route("/news/words")
+    def news_words():
+        url = request.args.get("url")
+        if url is None:
+            return render_template("news_words.html", msg="뉴스 랭킹 페이지에서 접근하세요.")
+            # return redirect("/news/ranking")
+            # return redirect("/")
+
+        driver.get(url)
+
+        soup = BS(driver.page_source, "html.parser")
+
+        content = soup.select(".article_view")[0].get_text()
+
+        # Konply pos : 품사 분석
+        words = kkma.pos(content)
+
+        # 일반명사: NNG, 고유명사 : NNP
+        words = [word for word, tag in words if tag == "NNG" or tag == "NNP"]
+        words = [
+            dict(word=word, count=words.count(word)) for word in set(words)
+        ]
+
+        words = sorted(words, key=lambda x: x.get("count"), reverse=True)
+
+        return render_template("news_words.html", words=words)
+
+    @app.route("/downloads/<keyword>")
+    def downloads(keyword):
+        url = f"https://www.google.com/search?q={keyword}&tbm=isch"
+        driver.get(url)
+        soup = BS(driver.page_source, "html.parser")
+
+        try:
+            for root, _, files in os.walk(f"static/download/{keyword}", topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+        except:
+            pass
+        os.makedirs(f"static/download/{keyword}", exist_ok=True)
+
+        img_list = []
+        # 이미지 파일(src)과 URL link(data-src)
+        img_links = [(tag.get("src"), tag.get("data-src"))
+                     for tag in soup.select(".rg_i")]
+        for i, (img_data, img_link) in enumerate(img_links):
+            ext = "jpg"
+            if img_data:
+                data = base64.b64decode(img_data.split(",")[1])
+
+                # 이미지 파일 확장자 확인
+                regex = re.compile("^data:image/([^;]+)")
+                ext = regex.findall(img_data)[0]
+            elif img_link:
+                res = requests.get(img_link)
+
+                # 이미지 파일 확장자 확인
+                data = res.content
+
+                regex = re.compile("^image/(.+)")
+                ext = regex.findall(res.headers.get("Content-Type"))[0]
+            else:
+                continue
+
+            # Data -> File, Link대신에 이미지 파일을 직접 화면에 출력하는 작업
+            filepath = f"static/download/{keyword}/{i}.{ext}"
+            img_list.append(f"/{filepath}")
+            with open(f"./{filepath}", "wb") as f:
+                f.write(data)
+
+        return render_template("downloads.html", img_link=img_list)
+
+    app.run(port=5005)
+
+    exit(0)
